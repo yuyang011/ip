0c6110f4641e6fca4842670c72ab777340855e45
yuyang011 <yuyang011@gmail.com>
2025-01-24 02:40:12 +0800

    Added Delete and updated input testcases

--- src/main/java/Ares.java
+++ src/main/java/Ares.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Ares {
     public static void main(String[] args) {
@@ -9,8 +10,8 @@ public class Ares {
         System.out.println(indent + "Its been awhile\n" + indent + "What can I do for you today?");
         System.out.println(indent + line);
 
-        Task[] tasks = new Task[100];
-        int tasksSize = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
+        int counter = 0;
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -23,40 +24,40 @@ public class Ares {
                     break;
                 } else if (input.equals("list")) {
                     System.out.println(indent + "Here are the tasks in your list:");
-                    for (int i = 0; i < tasksSize; i++){
-                        System.out.println(indent + (i + 1) + "." + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++){
+                        System.out.println(indent + (i + 1) + "." + tasks.get(i));
                     }
                     System.out.println(indent + line);
                 } else if (input.startsWith("mark")) {
                     int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskNum < 0 || taskNum > 100) {
+                    if (taskNum < 0) {
                         throw new AresException("You have entered an invalid number");
                     }
-                    if (tasks[taskNum].status()) {
+                    if (tasks.get(taskNum).status()) {
                         throw new AresException("You have already completed this task");
                     }
-                    tasks[taskNum].markAsDone();
+                    tasks.get(taskNum).markAsDone();
                     System.out.println(indent + "NICE, I have marked this task as completed:");
-                    System.out.println(indent + "  " + tasks[taskNum].toString());
+                    System.out.println(indent + "  " + tasks.get(taskNum).toString());
                     System.out.println(indent + line);
                 } else if (input.startsWith("unmark")) {
                     int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskNum < 0 || taskNum > 100) {
+                    if (taskNum < 0) {
                         throw new AresException("You have entered an invalid number");
                     }
-                    if (!tasks[taskNum].status()) {
+                    if (!tasks.get(taskNum).status()) {
                         throw new AresException("You have not completed this task in the first place");
                     }
-                    tasks[taskNum].markAsNotDone();
+                    tasks.get(taskNum).markAsNotDone();
                     System.out.println(indent + "Alright, I have marked this task as not completed yet:");
-                    System.out.println(indent + "  " + tasks[taskNum].toString());
+                    System.out.println(indent + "  " + tasks.get(taskNum).toString());
                     System.out.println(indent + line);
                 } else if (input.startsWith("todo") || input.startsWith("deadline") || input.startsWith("event")) {
                     if (input.startsWith("todo")) {
                         if (input.trim().equals("todo")) {
                             throw new AresException("STOP RIGHT THERE!!! You forgotten to write a description");
                         }
-                        tasks[tasksSize] = new Todo(input.substring(5));
+                        tasks.add(new Todo(input.substring(5)));
                     }
                     if (input.startsWith("deadline")) {
                         if (!input.trim().contains("/by")) {
@@ -66,7 +67,7 @@ public class Ares {
                         if (parts.length < 2 || parts[1].isEmpty()) {
                             throw new AresException("STOP RIGHT THERE!!! You forgotten to write a deadline timing");
                         }
-                        tasks[tasksSize] = new Deadline(parts[0].substring(9), parts[1]);
+                        tasks.add(new Deadline(parts[0].substring(9), parts[1]));
                     }
                     if (input.startsWith("event")) {
                         if ((!input.trim().contains("/from")) || (!input.trim().contains("/to"))) {
@@ -80,12 +81,22 @@ public class Ares {
                         if (subParts.length < 2) {
                             throw new AresException("STOP RIGHT THERE!!! You entered an incomplete event timing");
                         }
-                        tasks[tasksSize] = new Event(parts[0].substring(6), subParts[0], subParts[1]);
+                        tasks.add(new Event(parts[0].substring(6), subParts[0], subParts[1]));
                     }
-                    tasksSize++;
+                    counter++;
                     System.out.println(indent + "Got it. I've added this task:");
-                    System.out.println(indent + "  " + tasks[tasksSize - 1]);
-                    System.out.println(indent + "Now you have " + tasksSize + " tasks in the list.");
+                    System.out.println(indent + "  " + tasks.get(counter - 1));
+                    System.out.println(indent + "Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(indent + line);
+                } else if (input.startsWith("delete")) {
+                    int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (taskNum < 0) {
+                        throw new AresException("You have entered an invalid number");
+                    }
+                    Task deletedTask = tasks.remove(taskNum);
+                    System.out.println(indent + "Noted. I've removed this task:");
+                    System.out.println(indent + "  " + deletedTask);
+                    System.out.println(indent + "Now you have " + tasks.size() + " tasks in the list.");
                     System.out.println(indent + line);
                 } else {
                     throw new AresException("??? I do not understand what you entered");
