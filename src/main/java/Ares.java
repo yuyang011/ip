dba3e339facfc38a555d0966721850f57503f2ee
yuyang011 <yuyang011@gmail.com>
2025-01-23 20:23:20 +0800

    Renamed to Ares.java

---
+++ src/main/java/Ares.java
@@ -0,0 +1,11 @@
+public class Ares {
+    public static void main(String[] args) {
+        String line = "-------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Ares");
+        System.out.println("What can I do for you\n");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(line);
+    }
+}
