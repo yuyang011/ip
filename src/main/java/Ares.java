d9afc3fdda8692ca99e1ec5a4221a2c4a549c555
yuyang011 <yuyang011@gmail.com>
2025-01-23 21:27:05 +0800

    Added Echo

--- src/main/java/Ares.java
+++ src/main/java/Ares.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Ares {
     public static void main(String[] args) {
         String line = "-------------------------------------";
         System.out.println(line);
         System.out.println("Hello! I'm Ares");
-        System.out.println("What can I do for you\n");
+        System.out.println("Its been awhile\nWhat can I do for you today?\n");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println("Nice chatting with you today\nSee you again next time.\n");
+                System.out.println(line);
+                break;
+            }
+        }
     }
 }
